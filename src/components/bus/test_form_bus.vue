04f534fc7d5ee2da08365973b88dc18e7cf2e5bd
forbidden Betta <forbiddenBetta@gmail.com>
2022-03-25 12:27:35 +0100

    API call almost working

--- src/components/bus/test_form_bus.vue
+++ src/components/bus/test_form_bus.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="container">
+      <!-- <button @click="handleSubmit">aazazaz</button> -->
       <b-form @submit="onSubmit" @reset="onReset" v-if="show">
         <b-form-group
           id="input-group-1"
@@ -10,7 +11,7 @@
         >
           <b-form-input
             id="input-1"
-            v-model="form.email"
+            v-model="form_e.email"
             type="email"
             placeholder="Enter email"
             required
@@ -25,7 +26,7 @@
           <b-form-input
             id="input-2"
             type="password"
-            v-model="form.password"
+            v-model="form_e.password"
             placeholder="Enter password"
             required
           ></b-form-input>
@@ -35,38 +36,111 @@
         <b-button type="reset" variant="danger">Reset</b-button>
       </b-form>
       <b-card class="mt-3" header="Form Data Result">
-        <pre class="m-0">{{ form }}</pre>
+        <pre class="m-0">{{ form_e }}</pre>
       </b-card>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        email: "",
-        password: "",
-      },
-      show: true,
-    };
-  },
-  methods: {
-    onSubmit(event) {
-      event.preventDefault();
-      alert(JSON.stringify(this.form));
-    },
-    onReset(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.email = "";
-      this.form.password = ""; // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
-    },
-  },
-};
+<script setup>
+const form_e = { email: "", password: "" };
+const show = true;
+
+function onSubmit() {
+  // preventDefault();
+  alert(JSON.stringify(form_e));
+  handleSubmit();
+}
+
+function onReset(event) {
+  // event.preventDefault();
+  this.form_e.email = "";
+  this.form_e.password = "";
+  this.show = false;
+  this.$nextTick(() => {
+    this.show = true;
+  });
+}
+
+async function handleSubmit() {
+  console.log("handle");
+  console.log({ form_e });
+
+  let response = await fetch("https://apibus.quidam.re/api/login_check", {
+    method: "POST",
+    body: JSON.stringify({
+      email: form_e.email,
+      password: form_e.password,
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => err);
+
+  if (response.message === "") {
+    this.notification = "Article ajouter avec succès";
+    this.new_article = response.article_id;
+  }
+}
+
+
+
+
+// export default {
+//   data() {
+//     return {
+//       form: {
+//         email: "",
+//         password: "",
+//       },
+//       show: true,
+//     };
+//   },
+//   methods: {
+//     onSubmit(event) {
+//       event.preventDefault();
+//       alert(JSON.stringify(this.form));
+//     },
+//     onReset(event) {
+//       event.preventDefault();
+//       // Reset our form values
+//       this.form.email = "";
+//       this.form.password = ""; // Trick to reset/clear native browser form validation state
+//       this.show = false;
+//       this.$nextTick(() => {
+//         this.show = true;
+//       });
+//     },
+//     async work_api() {
+//       const response = await fetch("https://apibus.quidam.re/api/login_check");
+
+//       const answer = await response.json();
+//       console.log(answer);
+//       // const { data: users } = await response.json();
+
+//       // this.users = users;
+//     },
+
+//     async handleSubmit() {
+//       console.log("handle");
+
+//       let laform = new FormData();
+//       laform.append(form.email);
+//       laform.append(form.password);
+
+//       console.log({ laform });
+
+//       let response = await fetch("https://apibus.quidam.re/api/login_check", {
+//         method: "POST",
+//         body: laform,
+//       })
+//         .then((res) => res.json())
+//         .catch((err) => err);
+
+//       if (response.message === "") {
+//         this.notification = "Article ajouter avec succès";
+//         this.new_article = response.article_id;
+//       }
+//     },
+//   },
+// };
 </script>
